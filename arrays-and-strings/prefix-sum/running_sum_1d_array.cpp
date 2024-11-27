e0389958227796c9a6ff68fb52091d719cc25cf4
mauroarcidiacono <mauroarcidiacono@hotmail.com>
2024-11-27 21:02:45 +0000

    Modified the structure of the repo. Added the Running Sum of 1d Array problem.

---
+++ arrays-and-strings/prefix-sum/running_sum_1d_array.cpp
@@ -0,0 +1,32 @@
+// 1480. Running Sum of 1d Array
+// https://leetcode.com/problems/running-sum-of-1d-array/description/
+// Difficulty: Easy
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> prefix = {nums[0]};
+        for (int i = 1 ; i < nums.size(); i++) {
+            prefix.push_back(nums[i] + prefix[prefix.size() - 1]);
+        };
+
+        for (int num : prefix) {
+            cout << num << " ";
+        }
+
+        return prefix;
+    }
+};
+
+int main() {
+    vector<int> nums = {1,2,3,4};
+    Solution sol;
+    sol.runningSum(nums);
+
+    return 0;
+}
