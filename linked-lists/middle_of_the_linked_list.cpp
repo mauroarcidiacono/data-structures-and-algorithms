6abb845bc588947a7b2d899c05de9a0aa3032dba
mauroarcidiacono <mauroarcidiacono@hotmail.com>
2025-01-03 13:06:48 -0300

    Added the solution of problem 876.

---
+++ linked-lists/middle_of_the_linked_list.cpp
@@ -0,0 +1,61 @@
+// 876. Middle of the Linked List
+// https://leetcode.com/problems/middle-of-the-linked-list/
+// Difficulty: Easy
+// Strategy: fast and slow pointers.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        cout << slow->val << '\n';
+        return slow;
+    }
+};
+
+int main() {
+    Solution sol;
+    ListNode* head = new ListNode(-1);
+    ListNode* n0 = new ListNode(1);
+    ListNode* n1 = new ListNode(2);
+    ListNode* n2 = new ListNode(3);
+    ListNode* n3 = new ListNode(4);
+    ListNode* n4 = new ListNode(5);
+    ListNode* n5 = new ListNode(6);
+
+    head->next = n0;
+    n0->next = n1;
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = n5;
+
+    sol.middleNode(head);
+}
