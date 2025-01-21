2a31875505bffa0959f9cd28bc089cffed598ac8
mauroarcidiacono <mauroarcidiacono@hotmail.com>
2025-01-21 11:18:44 -0300

    Added the solution of problem 844.

---
+++ stacks-and-queues/backspace_string_compare.cpp
@@ -0,0 +1,35 @@
+// 844. Backspace String Compare
+// https://leetcode.com/problems/backspace-string-compare/description/
+// Difficulty: Easy.
+// Strategy: stack.
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool backspaceCompare (string s, string t) {
+        return build(s) == build(t);
+    }
+
+    string build(string s) {
+        string str = "";
+        for (char c : s) {
+            if (c != '#') {
+                str.push_back(c);
+            } else if (!str.empty()) {
+                str.pop_back();
+            } 
+        }
+        return str;
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "ab#c";
+    string t = "ad#c";
+    cout << (sol.backspaceCompare(s, t) ? "true" : "false") << '\n';
+
+    return 0;
+} 
